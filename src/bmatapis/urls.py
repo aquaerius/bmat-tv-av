9222de7908df943a8361b765f02efff131f5f9eb
Aquaerius SSL <33044201+aquaerius@users.noreply.github.com>
2020-02-24 00:10:11 +0100

    Update urls.py

--- src/bmatapis/urls.py
+++ src/bmatapis/urls.py
@@ -1,18 +1,3 @@
-"""bmatapis URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
@@ -26,11 +11,11 @@ from tv import views as tv_views
 
 
 router = routers.DefaultRouter()
-router.register(r'programs', tv_views.ShowtimeViewSet)
+router.register(r'programs', tv_views.ProgramViewSet)
+router.register(r'channels', tv_views.ChannelViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('^%s$' %settings.LOGIN_URL[1:], RedirectView(url='bmatapis/login/')),
     url(r'^login/$', auth_views.login, {'template_name': 'core/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^', include(core_urls)), 
